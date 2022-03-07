cc04a603b4c28fd3e5e9491dd965b4e9125bf122
John Climie <johnclimie00@gmail.com>
2022-03-07 16:23:06 -0500

    Created index.js file

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const db = require('');
+const routes = require('');
+
+const PORT = 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server is running on port ${PORT}`);
+    });
+});
