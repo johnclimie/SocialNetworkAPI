4e64676a3b575bb5b812ff9511454b83dc83cfa7
John Climie <johnclimie00@gmail.com>
2022-03-09 16:23:42 -0500

    Created routes and controllers to create, get all and get individual thoughts

---
+++ controllers/thoughtController.js
@@ -0,0 +1,45 @@
+const { Thought, User } = require('../models');
+
+module.exports = {
+    // Gets all thoughts
+    getThoughts(req, res) {
+        Thought.find()
+            .then((users) => res.json(users))
+            .catch((err) => res.status(500).json(err));
+    },
+
+    // Gets individual thought
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+            .select('-__v')
+            .then((thought) => 
+                !thought
+                    ? res.status(404).json({ message: 'No thought with this ID'})
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+
+    // Creates a  new thought
+    createThought(req, res) {
+        Thought.create(req.body)
+            .then((thought) => {
+                return User.findOneAndUpdate(
+                    { _id: req.body.userId },
+                    { $addToSet: { thoughts: thought._id } },
+                    { new: true }
+                );
+            })
+            .then((user) => 
+                !user
+                    ? res.status(404).JSON({
+                        message: 'Thought created, but found no user with this ID',
+                        })
+                    : res.json('Thought has been created')
+            )
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            })
+    }
+}
--- controllers/userController.js
+++ controllers/userController.js
@@ -60,11 +60,6 @@ module.exports = {
             { $addToSet: { friends: req.params.friendId } },
             { new: true }
         )
-        .findOneAndUpdate(
-            { _id: req.params.friendId },
-            { $addToSet: { friends: req.params.userId } },
-            { new: true }
-        )
         .then((user) => 
             !user
                 ? res.status(404).json({ message: 'No user with this ID' })
@@ -76,10 +71,10 @@ module.exports = {
     // Deletes a friend from a user's friend list
     deleteFriend(req, res) {
         User.findOneAndUpdate(
-            { _id: req.params.userId },
-            { $pull: { friends: { friendId: req.params.friendId } } },
-            { runValidators: true, new: true }
-        )
+                { _id: req.params.userId },
+                { $pull: { friends: { friendId: req.params.friendId } } },
+                { runValidators: true, new: true }
+            )
             .then((user) => 
                 !user
                     ? res.status(404).json({ message: 'No user with this id'})
